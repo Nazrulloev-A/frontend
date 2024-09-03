3a5eb203c3b2a21b0b63acdc9e73f385bc7a2489
Naz-Amer <smith2011@inbox.ru>
2024-09-03 16:23:42 -0500

    add react componets

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useContext } from "react";
+import { ThemeContext } from "./context/theme.context";
+import Navbar from "./components/navbar/Navbar";
 
-function App() {
+const App = () => {
+  const {darkMode} = useContext(ThemeContext);
+
+  const applyStyle = darkMode ? "app dark" : "app"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={applyStyle}> <h1> <Navbar/></h1>
+    <div className="wrapper">
+      Routes
     </div>
-  );
+    </div>
+    
+  )
 }
 
-export default App;
+export default App
